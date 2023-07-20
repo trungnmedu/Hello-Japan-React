75de8051bb339b51d8e0df702c8577d240042ad2
Minh Trung <nguyenminhtrungse@gmail.com>
2023-07-20 13:11:51 +0700

    Update about us

--- src/components/Home/Introduce/Introduce.jsx
+++ src/components/Home/Introduce/Introduce.jsx
@@ -11,7 +11,7 @@ const Introduce = () => {
                         <h1 className="font-semibold text-cyan-900 text-2xl">TRUNG TÂM NHẬT BẢN HELLO JAPAN</h1>
 
                         <h5 className="mt-2 font-medium italic text-cyan-900">Công Ty TNHH Tư Vấn Và Dịch Vụ Việt Nhật Hello Japan Kính Chào Quý Khách!</h5>
-                        <p className="mt-4">Trung tâm Du học Nhật Bản Hello Japan là đơn vị chuyên về tư vấn Du học Nhật Bản tại Việt Nam. Trong quá trình hoạt động của mình, Hello Japan luôn là một trong những thương hiệu có uy tín nhất theo đánh giá của Khách hàng và Đối tác. Trải qua quãng thời gian hơn 8 năm, hiện tại Hello Japan đã thành công trong việc giúp hơn 5000 học viên được học tập, sinh sống và làm việc tại Nhật Bản.</p>
+                        <p className="mt-4">Trung tâm Du học Nhật Bản Hello Japan là đơn vị chuyên về tư vấn Du học Nhật Bản tại Việt Nam. Trong quá trình hoạt động của mình, Hello Japan luôn là một trong những thương hiệu có uy tín nhất theo đánh giá của Khách hàng và Đối tác.</p>
                         <p className="mt-4">Đến với Trung tâm Hello Japan, các học viên sẽ luôn nhận được sự tư vấn nhiệt tình, hiệu quả cho từng trường hợp. Hello Japan cũng là đơn vị đầu tiên tại Việt Nam cam kết MIỄN 100% PHÍ TƯ VẤN và PHÍ XỬ LÝ HỒ SƠ cho học viên. Với TẦM NHÌN, SỨ MỆNH, và bộ GIÁ TRỊ CỐT LÕI được định hướng rõ ràng. Chúng tôi tự hào mang tới cho học viên sự hỗ trợ tốt nhất, là chỗ dựa vững chắc cho những bước chân đầu tiên hướng tới giấc mơ Du học Nhật Bản của học viên.</p>
                     </div>
                     <div className="col-span-4">
